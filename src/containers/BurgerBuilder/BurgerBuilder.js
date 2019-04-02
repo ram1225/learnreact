d3f053be6db47c09710ea0ab94b2d7519139fba6
ram1225 <geek.emp25@gmail.com>
2019-04-02 22:11:31 +0530

    first changes

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Aux";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.5
+}
+
+class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 4,
+    purchasable:false
+  };
+
+  updatePurchaseState(){
+    
+  }
+
+  addIngHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount+1;
+    const updatedIng = {
+        ...this.state.ingredients
+    };
+    updatedIng[type]= updatedCount;
+
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({
+        totalPrice: newPrice,
+        ingredients: updatedIng
+    });
+
+  }
+
+  removeIngHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if(oldCount <= 0) return;
+    const updatedCount = oldCount-1;
+    const updatedIng = {
+        ...this.state.ingredients
+    };
+    updatedIng[type]= updatedCount;
+
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({
+        totalPrice: newPrice,
+        ingredients: updatedIng
+    });
+  };
+  render() {
+      const disableInfo = {
+          ...this.state.ingredients
+      };
+      for(let key in disableInfo){
+          disableInfo[key]= disableInfo[key]<=0;
+      }
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls
+        ingAdded={this.addIngHandler} 
+        ingReduced={this.removeIngHandler}
+        disabled={disableInfo}
+        price={this.state.totalPrice}/>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
